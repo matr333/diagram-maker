052e95f7cf686302d94e3669cb5073d7c37a04fa
Eugen <yauheni.ivashkevich@glympse.com>
2023-12-01 23:10:16 +0300

    fix: test fixes

--- src/state/node/nodeActionDispatcher.ts
+++ src/state/node/nodeActionDispatcher.ts
@@ -206,7 +206,7 @@ export function handlePotentialNodeDragStart<NodeType, EdgeType>(
   const typeId = target.id;
   if (typeId) {
     const size = getSizeFromDataAttrs(target) || config.getSizeForNodeType(typeId);
-    const consumerData = JSON.parse(target.originalTarget.getAttribute('data-consumer-data') || '{}');
+    const consumerData = JSON.parse(target.originalTarget.getAttribute('data-consumer-data') || 'null');
     if (size) {
       const centeredPosition = getCenteredPosition(position, size);
       const action = createPotentialNodeDragStartAction(typeId, centeredPosition, size, consumerData);
